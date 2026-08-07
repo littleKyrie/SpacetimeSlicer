46e548ae76afd66439038b8ff2e8791e1ac18579
zhanghanyin <zhanghanyin@mgtv.com>
2026-08-07 11:31:46 +0800

    调整rvm下采样比例为1.0

--- models/rvm.py
+++ models/rvm.py
@@ -23,6 +23,6 @@ class RVMStrategy(SegmentationStrategy):
     def process_frame(self, current_img, current_idx):
         tensor_img = bgr_frame_to_tensor(current_img).unsqueeze(0).to(self.device)
         with torch.no_grad():
-            fgr, pha, *self.rec = self.model(tensor_img, *self.rec, downsample_ratio=0.25)
+            fgr, pha, *self.rec = self.model(tensor_img, *self.rec, downsample_ratio=1.0)
         alpha = (pha[0].cpu().numpy().transpose(1, 2, 0) * 255).astype(np.uint8)[:, :, 0]
         return alpha
